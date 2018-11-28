2eeeb8996b832df3b539e7a69276a1137d0a1f72
Kristy Ong <kristy.mil.ong@gmail.com>
2018-11-28 16:40:35 -0700

    finished blog

---
+++ app.js
@@ -0,0 +1,25 @@
+var timeForPost = document.getElementById('postTime')
+
+// utc
+var today2 = Date.now();
+
+//console.log(today)
+//console.log(today2)
+
+function setTime() {
+  // today
+  var today = new Date();
+
+  var day = today.getDate()
+  var month = today.getMonth() + 1
+  var year = today.getFullYear()
+
+  // format the output
+  //11-28-2018
+  var date = month + '-' + day + '-' + year 
+
+  // display on html
+  timeForPost.innerHTML = date
+}
+
+setTime()
